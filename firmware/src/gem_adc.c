94243267b1446bc2bb0db420870d2e5cb7c1d491
Thea Flowers <me@thea.codes>
2020-08-11 20:50:32 -0400

    Move ADC config to configuration header

--- firmware/src/gem_adc.c
+++ firmware/src/gem_adc.c
@@ -31,53 +31,56 @@ void gem_adc_init() {
     /* Wait for bus synchronization. */
     while (ADC->STATUS.bit.SYNCBUSY) {};
 
-    /* Use the internal VCC reference. This is 1/2 of what's on VCCA.
-    since VCCA is typically 3.3v, this is 1.65v.
-    */
-    ADC->REFCTRL.reg = ADC_REFCTRL_REFSEL_INTVCC1;
+    ADC->CTRLB.reg |= GEM_ADC_PRESCALER;
 
-    /* Only capture one sample. The ADC can actually capture and average multiple
-    samples for better accuracy, but there's no need to do that for this
-    example.
-    */
-    ADC->AVGCTRL.reg = ADC_AVGCTRL_SAMPLENUM_1;
+    ADC->AVGCTRL.reg |= GEM_ADC_SAMPLE_NUM | GEM_ADC_SAMPLE_ADJRES;
 
-    /* Set the clock prescaler to 512, which will run the ADC at
-    8 Mhz / 512 = 31.25 kHz.
-    Set the resolution to 12bit.
-    */
-    ADC->CTRLB.reg = ADC_CTRLB_PRESCALER_DIV512 |
-                    ADC_CTRLB_RESSEL_12BIT;
+    #if GEM_ADC_SAMPLE_NUM == ADC_AVGCTRL_SAMPLENUM_1
+        ADC->CTRLB.reg |= ADC_CTRLB_RESSEL_12BIT;
+    #else
+        // 16-bit result needed for multisampling
+        ADC->CTRLB.reg |= ADC_CTRLB_RESSEL_16BIT;
+    #endif
 
-    /* Configure the input parameters.
+    /* Configure the measurement parameters. */
 
-    - GAIN_DIV2 means that the input voltage is halved. This is important
-        because the voltage reference is 1/2 of VCCA. So if you want to
-        measure 0-3.3v, you need to halve the input as well.
 
-    - MUXNEG_GND means that the ADC should compare the input value to GND.
+    #ifndef GEM_ADC_USE_EXTERNAL_REF
+        /*
+        - Use the internal VCC reference. This is 1/2 of what's on VCCA.
+            since VCCA is typically 3.3v, this is 1.65v.
+        - GAIN_DIV2 means that the input voltage is halved. This is important
+            because the voltage reference is 1/2 of VCCA. So if you want to
+            measure 0-3.3v, you need to halve the input as well.
+        */
+        ADC->REFCTRL.reg |= ADC_REFCTRL_REFSEL_INTVCC1;
+        ADC->INPUTCTRL.reg |= ADC_INPUTCTRL_GAIN_DIV2 | ADC_INPUTCTRL_MUXNEG_GND;
+    #else
+        #error External ref code not written yet
+    #endif
 
-    - MUXPOST_PIN3 means that the ADC should read from AIN3, or PB09.
-        This is A2 on the Feather M0 board.
-    */
-    ADC->INPUTCTRL.reg = ADC_INPUTCTRL_GAIN_DIV2 |
-                        ADC_INPUTCTRL_MUXNEG_GND |
-                        ADC_INPUTCTRL_MUXPOS_PIN3;
-
-    // /* Set PB09 as an input pin. */
-    // PORT->Group[1].DIRCLR.reg = PORT_PB09;
-
-    // /* Enable the peripheral multiplexer for PB09. */
-    // PORT->Group[1].PINCFG[9].reg |= PORT_PINCFG_PMUXEN;
-
-    // /* Set PB09 to function B which is analog input. */
-    // PORT->Group[1].PMUX[4].reg = PORT_PMUX_PMUXO_B;
 
     /* Wait for bus synchronization. */
     while (ADC->STATUS.bit.SYNCBUSY) {};
 
     /* Enable the ADC. */
     ADC->CTRLA.bit.ENABLE = true;
+
+    /*
+    - MUXPOS_PIN3 means that the ADC should read from AIN3, or PB09.
+        This is A2 on the Feather M0 board.
+    */
+    ADC->INPUTCTRL.reg |= ADC_INPUTCTRL_MUXPOS_PIN3;
+
+    /* Set PB09 as an input pin. */
+    PORT->Group[1].DIRCLR.reg = PORT_PB09;
+
+    /* Enable the peripheral multiplexer for PB09. */
+    PORT->Group[1].PINCFG[9].reg |= PORT_PINCFG_PMUXEN;
+
+    /* Set PB09 to function B which is analog input. */
+    PORT->Group[1].PMUX[4].reg = PORT_PMUX_PMUXO_B;
+
 
     /* Make one read and throw it away, as per the datasheet */
     gem_adc_read_sync();
