8b6915d90df5c0230c7f27ebb626594312212ff4
Thea Flowers <me@thea.codes>
2020-08-12 22:30:49 -0400

    Add SEGGER RTT & printf

--- firmware/src/gem_config.c
+++ firmware/src/gem_config.c
@@ -1,7 +1,29 @@
+#include <stdarg.h>
+#include <stdio.h>
 #include "gem_config.h"
 
+#ifdef DEBUG
+#include "SEGGER_RTT.h"
+#endif
 
 const struct gem_adc_input gem_adc_inputs[] = {
     {1, PORT_PB09, ADC_INPUTCTRL_MUXPOS_PIN3},
     {1, PORT_PB08, ADC_INPUTCTRL_MUXPOS_PIN2},
 };
+
+
+void gem_config_init() {
+}
+
+int __wrap_printf(const char * format, ...) {
+    #ifdef DEBUG
+        va_list args;
+        va_start(args, format);
+        int result = SEGGER_RTT_vprintf(0, format, &args);
+        va_end(args);
+        return result;
+    #else
+        (void)format;
+        return 0;
+    #endif
+}
