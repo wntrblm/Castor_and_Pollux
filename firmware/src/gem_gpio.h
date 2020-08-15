8f16e3a4e16dd45078da5546c470d44d0ff95b1e
Thea Flowers <me@thea.codes>
2020-08-14 23:20:25 -0400

    Add gem_gpio_set_mux

--- firmware/src/gem_gpio.h
+++ firmware/src/gem_gpio.h
@@ -4,6 +4,15 @@
 #include <stdbool.h>
 #include "sam.h"
 
+#define GEM_PMUX_A PORT_PMUX_PMUXE_A_Val
+#define GEM_PMUX_B PORT_PMUX_PMUXE_B_Val
+#define GEM_PMUX_C PORT_PMUX_PMUXE_C_Val
+#define GEM_PMUX_D PORT_PMUX_PMUXE_D_Val
+#define GEM_PMUX_E PORT_PMUX_PMUXE_E_Val
+#define GEM_PMUX_F PORT_PMUX_PMUXE_F_Val
+#define GEM_PMUX_G PORT_PMUX_PMUXE_G_Val
+#define GEM_PMUX_H PORT_PMUX_PMUXE_H_Val
+
 
 static inline void gem_gpio_set_as_output(uint8_t port, uint8_t pin) {
     PORT->Group[port].DIRSET.reg = (1 << pin);
@@ -12,7 +21,7 @@ static inline void gem_gpio_set_as_output(uint8_t port, uint8_t pin) {
 static inline void gem_gpio_set_as_input(uint8_t port, uint8_t pin, bool pullup) {
     PORT->Group[port].DIRCLR.reg = (1 << pin);
     PORT->Group[port].PINCFG[pin].reg |= PORT_PINCFG_INEN;
-    if(pullup) PORT->Group[port].PINCFG[pin].reg |= PORT_PINCFG_PULLEN ;
+    if(pullup) PORT->Group[port].PINCFG[pin].reg |= PORT_PINCFG_PULLEN;
 }
 
 static inline void gem_gpio_set(uint8_t port, uint8_t pin, bool value) {
@@ -25,4 +34,13 @@ static inline void gem_gpio_set(uint8_t port, uint8_t pin, bool value) {
 
 static inline bool gem_gpio_get(uint8_t port, uint8_t pin) {
     return PORT->Group[port].IN.reg & (1 << pin);
+}
+
+static inline void gem_gpio_set_mux(uint8_t port, uint8_t pin, uint8_t mux) {
+    PORT->Group[port].PINCFG[pin].reg |= PORT_PINCFG_PMUXEN;
+    if(pin & 1) {
+        PORT->Group[port].PMUX[pin >> 1].bit.PMUXO = mux;
+    } else {
+        PORT->Group[port].PMUX[pin >> 1].bit.PMUXE = mux;
+    }
 }
