7d5969cf50723487924773183b417080cf2c1400
Thea Flowers <me@thea.codes>
2020-08-17 20:14:30 -0400

    Add basic driver for the mcp4728

---
+++ firmware/src/gem_mcp4728.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "gem_i2c.h"
+
+enum gem_i2c_result gem_mcp_4728_write_channel(uint8_t channel, uint16_t value);
+enum gem_i2c_result gem_mcp_4728_write_channels(uint16_t ch_a_value, uint16_t ch_b_value, uint16_t ch_c_value, uint16_t ch_d_value);
