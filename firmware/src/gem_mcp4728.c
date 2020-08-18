47f907b143308e9ae9d911963a019f4d9ca03025
Thea Flowers <me@thea.codes>
2020-08-17 20:14:30 -0400

    Add basic driver for the mcp4728

---
+++ firmware/src/gem_mcp4728.c
@@ -0,0 +1,35 @@
+#include "gem_mcp4728.h"
+#include "gem_i2c.h"
+
+
+#define I2C_ADDRESS 0x60
+#define SINGLE_WRITE_CMD 0b01011000
+#define SEQUENTIAL_WRITE_CMD 0b01010000
+
+
+enum gem_i2c_result gem_mcp_4728_write_channel(uint8_t channel, uint16_t value) {
+    uint8_t data[3] = {
+        SINGLE_WRITE_CMD | ((channel & 0x3) << 1),
+        (value >> 8) & 0xF,
+        (value & 0xFF),
+    };
+
+    return gem_i2c_write(I2C_ADDRESS, data, 3);
+}
+
+
+enum gem_i2c_result gem_mcp_4728_write_channels(uint16_t ch_a_value, uint16_t ch_b_value, uint16_t ch_c_value, uint16_t ch_d_value) {
+    uint8_t data[9] = {
+        SEQUENTIAL_WRITE_CMD,
+        (ch_a_value >> 8) & 0xF,
+        (ch_a_value & 0xFF),
+        (ch_b_value >> 8) & 0xF,
+        (ch_b_value & 0xFF),
+        (ch_c_value >> 8) & 0xF,
+        (ch_c_value & 0xFF),
+        (ch_d_value >> 8) & 0xF,
+        (ch_d_value & 0xFF),
+    };
+
+    return gem_i2c_write(I2C_ADDRESS, data, 9);
+}
--- firmware/src/main.c
+++ firmware/src/main.c
@@ -5,6 +5,7 @@
 #include "gem_adc.h"
 #include "gem_pulseout.h"
 #include "gem_i2c.h"
+#include "gem_mcp4728.h"
 #include "gem_config.h"
 
 
@@ -26,36 +27,30 @@ int main(void) {
 
     init_pins();
 
+    /* Configure the ADC and channel scanning. */
     gem_adc_init();
     gem_adc_init_input(&gem_adc_inputs[0]);
     gem_adc_init_input(&gem_adc_inputs[1]);
     gem_adc_start_scanning(gem_adc_inputs, 2, adc_results);
 
-
+    /* Configure the timers/PWM generators. */
     gem_pulseout_init();
     gem_pulseout_set_frequency(0, 2^24);
     gem_pulseout_set_duty(0, 0);
     gem_pulseout_set_frequency(1, 2^24);
     gem_pulseout_set_duty(1, 0);
 
+    /* Enable i2c bus for communicating with the DAC. */
     gem_i2c_init();
 
-    uint8_t i2c_address = 0x60;
     while(1) {
         if(gem_adc_results_ready()) {
             printf("Ch1: %lu, Ch2: %lu\r\n", adc_results[0], adc_results[1]);
 
             gem_pulseout_set_duty(0, adc_results[0] / 4096.0f);
-            gem_pulseout_set_duty(1, adc_results[1] / 4096.0f);
+            gem_pulseout_set_duty(1, adc_results[1] / 4096.0f);s
 
-
-            uint8_t i2c_data[] = {
-                0b01011010,
-                (adc_results[1] >> 8),
-                (adc_results[1] & 0xFF),
-            };
-
-            gem_i2c_write(i2c_address, i2c_data, 3);
+            gem_mcp_4728_write_channels(adc_results[0], adc_results[1], adc_results[0], adc_results[1]);
         }
     }
 
