a38021473652c6a7d165c7f7462f589b9457a67d
Thea Flowers <me@thea.codes>
2020-08-23 20:03:28 -0400

    Rename gem_pulseout_set_frequeny to set_period

--- firmware/src/gem_pulseout.h
+++ firmware/src/gem_pulseout.h
@@ -6,6 +6,6 @@
 
 void gem_pulseout_init();
 
-void gem_pulseout_set_frequency(uint8_t channel, uint32_t frequency);
+void gem_pulseout_set_period(uint8_t channel, uint32_t period);
 
 void gem_pulseout_set_duty(uint8_t channel, float duty);
