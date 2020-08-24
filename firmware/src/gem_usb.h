cd0909274d67db51c068d4f410d1c55402c176da
Thea Flowers <me@thea.codes>
2020-08-24 10:36:10 -0400

    Add usb midi send commands

--- firmware/src/gem_usb.h
+++ firmware/src/gem_usb.h
@@ -5,3 +5,5 @@ void gem_usb_init();
 void gem_usb_task();
 
 bool gem_usb_midi_receive(uint8_t packet[4]);
+bool gem_usb_midi_send(uint8_t packet[4]);
+size_t gem_usb_midi_send_bytes(uint8_t *data, size_t len);
