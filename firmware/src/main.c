158d75d4bc7afe40468eb7978b54e18a27538e68
Thea Flowers <me@thea.codes>
2020-08-31 22:31:02 -0400

    Update main.c for real hardware

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -19,7 +19,7 @@ static void init_pins() {
     // gem_gpio_set_as_output(PIN_STATUS_LED_2_PORT, PIN_STATUS_LED_2);
 }
 
-static uint32_t adc_results[2];
+static uint32_t adc_results[10];
 
 int main(void) {
     /* Configure clocks. */
@@ -42,14 +42,22 @@ int main(void) {
     gem_adc_init();
     gem_adc_init_input(&gem_adc_inputs[0]);
     gem_adc_init_input(&gem_adc_inputs[1]);
-    gem_adc_start_scanning(gem_adc_inputs, 2, adc_results);
+    gem_adc_init_input(&gem_adc_inputs[2]);
+    gem_adc_init_input(&gem_adc_inputs[3]);
+    gem_adc_init_input(&gem_adc_inputs[4]);
+    gem_adc_init_input(&gem_adc_inputs[5]);
+    gem_adc_init_input(&gem_adc_inputs[6]);
+    gem_adc_init_input(&gem_adc_inputs[7]);
+    gem_adc_init_input(&gem_adc_inputs[8]);
+    gem_adc_init_input(&gem_adc_inputs[9]);
+    gem_adc_start_scanning(gem_adc_inputs, 10, adc_results);
 
     /* Configure the timers/PWM generators. */
     gem_pulseout_init();
-    gem_pulseout_set_period(0, 0xFF);
-    gem_pulseout_set_duty(0, 0);
-    gem_pulseout_set_period(1, 0xFF);
-    gem_pulseout_set_duty(1, 0);
+    gem_pulseout_set_period(0, 1135);
+    gem_pulseout_set_duty(0, 0.5f);
+    gem_pulseout_set_period(1, 567);
+    gem_pulseout_set_duty(1, 0.5f);
 
     /* Enable i2c bus for communicating with the DAC. */
     gem_i2c_init();
@@ -69,21 +77,36 @@ int main(void) {
 
     /* Local variables */
     struct gem_voice_params castor_params;
+    struct gem_voice_params pollux_params;
+
+    /* Test */
+
+    // gem_voice_params_from_adc_code(
+    //     gem_voice_param_table, gem_voice_param_table_len, 1536, &castor_params);
+    
+    // gem_pulseout_set_period(0, castor_params.period_reg);
+    // gem_pulseout_set_duty(0, 0.5f);
+
+    // gem_mcp_4728_write_channels(castor_params.dac_code, 2048, 0, 0);
 
     while (1) {
         gem_usb_task();
         gem_midi_task();
 
         if (gem_adc_results_ready()) {
-            // printf("Ch1: %lu, Ch2: %lu\r\n", adc_results[0], adc_results[1]);
+            //printf("CV A: %lu, CV A Pot: %lu, CV B: %lu, CV B Pot: %lu, Duty A: %lu, Duty A Pot: %lu, Duty B: %lu, Duty B Pot: %lu, Phase: %lu, Phase Pot: %lu \r\n", adc_results[0], adc_results[1], adc_results[2], adc_results[3], adc_results[4], adc_results[5], adc_results[6], adc_results[7], adc_results[8], adc_results[9]);
 
-            gem_pulseout_set_duty(0, adc_results[0] / 4096.0f);
-            gem_pulseout_set_duty(1, adc_results[1] / 4096.0f);
-
-            gem_voice_params_from_adc_code(
-                gem_voice_param_table, gem_voice_param_table_len, adc_results[1], &castor_params);
-
-            // gem_mcp_4728_write_channels(adc_results[0], castor_params.dac_code, adc_results[0], adc_results[1]);
+            // gem_voice_params_from_adc_code(
+            //     gem_voice_param_table, gem_voice_param_table_len, 853, &castor_params);
+            // gem_voice_params_from_adc_code(
+            //     gem_voice_param_table, gem_voice_param_table_len, 853, &pollux_params);
+            
+            // gem_pulseout_set_period(0, castor_params.period_reg);
+            // gem_pulseout_set_duty(0, 0.5f);
+            // gem_pulseout_set_period(1, pollux_params.period_reg);
+            // gem_pulseout_set_duty(1, 0.5f);
+        
+            // gem_mcp_4728_write_channels(450, 4096 - adc_results[5], pollux_params.dac_code, 4096 - adc_results[7]);
         }
     }
 
