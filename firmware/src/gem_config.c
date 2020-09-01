55234901251024181b34400eebc869b704bea0fb
Thea Flowers <me@thea.codes>
2020-08-31 22:27:12 -0400

    Fixup analog input pins

--- firmware/src/gem_config.c
+++ firmware/src/gem_config.c
@@ -10,8 +10,16 @@
 #define NVM_SETTINGS_MARKER 0x63
 
 const struct gem_adc_input gem_adc_inputs[] = {
-    {1, PORT_PB09, ADC_INPUTCTRL_MUXPOS_PIN3},
-    {1, PORT_PB08, ADC_INPUTCTRL_MUXPOS_PIN2},
+    {0, 6, ADC_INPUTCTRL_MUXPOS_PIN6},  // CV A
+    {1, 3, ADC_INPUTCTRL_MUXPOS_PIN11}, // CV A Pot
+    {0, 7, ADC_INPUTCTRL_MUXPOS_PIN7},  // CV B
+    {1, 2, ADC_INPUTCTRL_MUXPOS_PIN10}, // CV B Pot
+    {0, 5, ADC_INPUTCTRL_MUXPOS_PIN5},  // Duty A
+    {0, 8, ADC_INPUTCTRL_MUXPOS_PIN16}, // Duty A Port
+    {1, 9, ADC_INPUTCTRL_MUXPOS_PIN3},  // Duty B
+    {0, 9, ADC_INPUTCTRL_MUXPOS_PIN17}, // Duty B Pot
+    {1, 8, ADC_INPUTCTRL_MUXPOS_PIN2},  // Phase, not working
+    {0, 2, ADC_INPUTCTRL_MUXPOS_PIN0},  // Phase pot
 };
 
 void gem_config_init() {}
