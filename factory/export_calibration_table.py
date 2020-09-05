6ac0fa77848b90b67ebc0ce948febecd94e46126
Thea Flowers <me@thea.codes>
2020-09-04 20:42:24 -0400

    Write up a simple calibration program and update the calibration table with real-world measurements

--- factory/export_calibration_table.py
+++ factory/export_calibration_table.py
@@ -12,4 +12,4 @@ with open("calibration-table.csv", "r") as fh:
 entries.reverse()
 
 for row in entries:
-    print(f"{{.adc_code = {row['adc code']}, .period_reg = {row['period reg']}, .dac_code = {row['dac code']} }},")
+    print(f"{{.adc_code = {row['adc code']}, .period_reg = {row['period reg']}, .dac_code = {row['calibrated dac code']} }},")
