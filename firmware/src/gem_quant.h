6eb3b24f3bb8a0b0dd4564b3be7a6ab53747f18f
Thea Flowers <me@thea.codes>
2020-09-08 18:17:35 -0400

    Move CV quant constant to config.h

--- firmware/src/gem_quant.h
+++ firmware/src/gem_quant.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "math.h"
 #include "stdint.h"
+#include "gem_config.h"
+
 
 inline static float _gem_quant(float delta, float value) { return delta * floor(value / delta * 0.5); }
 
 inline static float gem_quant_pitch_cv(uint16_t value) {
-    /* TODO: Make this a config def. */
-    /* 83.33 milivolts per semitone / 20 steps */
-    return _gem_quant((0.0833f / 20.0f), value);
+    return _gem_quant(GEM_QUANT_CV_STEP, value);
 }
