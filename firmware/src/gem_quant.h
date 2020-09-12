bc971882cecff3749cd644041a6fab0e57e70a56
Thea Flowers <me@thea.codes>
2020-09-12 13:03:03 -0400

    Fix quantization algorithm

--- firmware/src/gem_quant.h
+++ firmware/src/gem_quant.h
@@ -3,6 +3,6 @@
 #include "math.h"
 #include "stdint.h"
 
-inline static float _gem_quant(float delta, float value) { return delta * floor(value / delta * 0.5); }
+inline static float _gem_quant(float delta, float value) { return delta * roundf(value / delta); }
 
-inline static float gem_quant_pitch_cv(uint16_t value) { return _gem_quant(GEM_QUANT_CV_STEP, value); }
+inline static float gem_quant_pitch_cv(float value) { return _gem_quant(GEM_QUANT_CV_STEP, value); }
