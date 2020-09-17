de3c17c198b9ba1931087372d4b9ca787e7f2cc2
Thea Flowers <me@thea.codes>
2020-09-16 21:11:40 -0400

    Add naive and untested SPI controller implementation

---
+++ firmware/src/gem_spi.c
@@ -0,0 +1,37 @@
+#include "gem_spi.h"
+#include "gem_config.h"
+#include "gem_gpio.h"
+#include "sam.h"
+
+void gem_spi_init() { /* Enable the APB clock for SERCOM. */
+    PM->APBCMASK.reg |= GEM_SPI_SERCOM_APBCMASK;
+
+    /* Enable GCLK1 for the SERCOM */
+    GCLK->CLKCTRL.reg = GCLK_CLKCTRL_CLKEN | GEM_SPI_GCLK | GEM_SPI_GCLK_CLKCTRL_ID;
+
+    /* Wait for bus synchronization. */
+    while (GCLK->STATUS.bit.SYNCBUSY) {};
+
+    /* Configure pins for the correct function. */
+    gem_gpio_set_mux(GEM_SPI_SCK_PORT, GEM_SPI_SCK_PIN, GEM_SPI_SCK_PIN_FUNC);
+    gem_gpio_set_mux(GEM_SPI_SDO_PORT, GEM_SPI_SDO_PIN, GEM_SPI_SDO_PIN_FUNC);
+
+    /* Setup SPI controller. */
+    GEM_SPI_SERCOM->SPI.CTRLA.reg = SERCOM_SPI_CTRLA_MODE_SPI_MASTER | SERCOM_SPI_CTRLA_DOPO(GEM_SPI_DOPO);
+
+    if (GEM_SPI_POLARITY)
+        GEM_SPI_SERCOM->SPI.CTRLA.bit.CPOL = 1;
+    if (GEM_SPI_PHASE)
+        GEM_SPI_SERCOM->SPI.CTRLA.bit.CPHA = 1;
+
+    /* Enable the SERCOM. */
+    GEM_SPI_SERCOM->SPI.CTRLA.bit.ENABLE = 1;
+    while (GEM_SPI_SERCOM->SPI.SYNCBUSY.bit.ENABLE) {};
+}
+
+void gem_spi_write(uint8_t* data, size_t len) {
+    for (size_t i = 0; i < len; i++) {
+        while (!GEM_SPI_SERCOM->SPI.INTFLAG.bit.DRE) {}
+        GEM_SPI_SERCOM->SPI.DATA.reg = data[i];
+    }
+}
