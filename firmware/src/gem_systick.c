4d838c816d948c9125fd830e89ec37e3c78d16a0
Thea Flowers <me@thea.codes>
2020-09-19 02:42:04 -0400

    Use F_CPU instead of a magic number for systick config

--- firmware/src/gem_systick.c
+++ firmware/src/gem_systick.c
@@ -5,7 +5,7 @@ uint32_t _ms_ticks;
 
 void gem_systick_init() {
     /* Our system clock runs at 48 MHz, so this is 1ms per tick. */
-    SysTick_Config(48000000 / 1000);
+    SysTick_Config(F_CPU / 1000);
 }
 
 uint32_t gem_get_ticks() { return _ms_ticks; }
