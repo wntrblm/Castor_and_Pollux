b0239f8e2d06f88b9d68e0f3b5bb1d2f4626e525
Thea Flowers <me@thea.codes>
2020-09-20 21:30:02 -0400

    Set ADC gain and offset error via shell

--- factory/shell.py
+++ factory/shell.py
@@ -1,7 +1,7 @@
 import rtmidi
 import rtmidi.midiutil
 
-MIDI_PORT_NAME = "TinyUSB Device"
+MIDI_PORT_NAME = "Gemini"
 SYSEX_START = 0xF0
 SYSEX_END = 0xF7
 
@@ -12,10 +12,6 @@ port_in, _ = rtmidi.midiutil.open_midiport(MIDI_PORT_NAME, type_="input")
 port_out, _ = rtmidi.midiutil.open_midiport(MIDI_PORT_NAME, type_="output")
 
 port_in.ignore_types(sysex=False)
-
-#port_out.send_message([SYSEX_START, 0x77, 0x01, 0x01, 0x0F, 0x0F, 0x0F, SYSEX_END])
-#port_out.send_message([SYSEX_START, 0x77, 0x02, 0x0A, 0x0C, 0x0A, 0x0B, SYSEX_END])
-#port_out.send_message([SYSEX_START, 0x77, 0x03, 0x0B, 0x0A, 0x0C, 0x0A, SYSEX_END])
 
 
 def callback(event, data=None):
@@ -43,7 +39,11 @@ while True:
     if cmd == "f":
         port_out.send_message([SYSEX_START, 0x77, 0x07, 0x00, (val >> 12) & 0xF, (val >> 8) & 0xF, (val >> 4) & 0xF, val & 0xF, SYSEX_END])
         
-    
+    if cmd == "adcgain":
+        port_out.send_message([SYSEX_START, 0x77, 0x02, (val >> 12) & 0xF, (val >> 8) & 0xF, (val >> 4) & 0xF, val & 0xF, SYSEX_END])
+        
+    if cmd == "adcerror":
+        port_out.send_message([SYSEX_START, 0x77, 0x03, (val >> 12) & 0xF, (val >> 8) & 0xF, (val >> 4) & 0xF, val & 0xF, SYSEX_END])
     # while True:
     #     msg = port_in.get_message()
     #     if not msg:
