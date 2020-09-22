db79cddb42885d522da402a3a296f1d8b181cbd4
Thea Flowers <me@thea.codes>
2020-09-22 17:58:52 -0400

    Move LED animations to their own file

---
+++ firmware/src/gem_led_animation.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void gem_led_animation_init();
+
+void gem_led_animation_step();
--- firmware/src/gem_waveforms.h
+++ firmware/src/gem_waveforms.h
@@ -3,9 +3,9 @@
 #include "fix16.h"
 #include "math.h"
 
-float gem_sine(fix16_t phase) { return fix16_sin(fix16_mul(phase, F16(2.0f * 3.14159265f))); }
+static inline fix16_t gem_sine(fix16_t phase) { return fix16_sin(fix16_mul(phase, F16(2.0f * 3.14159265f))); }
 
-float gem_triangle(fix16_t phase) {
+static inline fix16_t gem_triangle(fix16_t phase) {
     /* -1.0f + fabsf(-2.0f + fmodf(phase, 1.0f) * 4.0f); */
     return fix16_add(F16(-1.0f), fix16_abs(fix16_add(F16(-2.0f), fix16_mul(F16(4.0f), fix16_mod(phase, F16(1.0f))))));
 }
