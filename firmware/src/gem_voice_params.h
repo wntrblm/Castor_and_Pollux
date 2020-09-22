e2df06ef3900e9c427988e100e42ea6b66f701a0
Thea Flowers <me@thea.codes>
2020-09-22 01:14:00 -0400

    Use fix16 for CV -> voice param calculation.

--- firmware/src/gem_voice_params.h
+++ firmware/src/gem_voice_params.h
@@ -2,6 +2,7 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <fix16.h>
 
 /* This defines the structure and functions used
    to map input CV to the required frequency and charge voltage.
@@ -10,11 +11,11 @@
 */
 
 struct gem_voice_params {
-    float voltage;
+    fix16_t voltage;
     uint32_t period_reg;
     uint16_t castor_dac_code;
     uint16_t pollux_dac_code;
 };
 
 void gem_voice_params_from_cv(
-    struct gem_voice_params* table, size_t table_len, float voltage, struct gem_voice_params* out);
+    struct gem_voice_params* table, size_t table_len, fix16_t voltage, struct gem_voice_params* out);
