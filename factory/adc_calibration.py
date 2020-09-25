0a6ad3ebfd65b60df2ee4177303bce3fe1397e17
Thea Flowers <me@thea.codes>
2020-09-24 23:50:02 -0400

    Fixup some issues found after testing calibration scripts on real hardware

--- factory/adc_calibration.py
+++ factory/adc_calibration.py
@@ -7,7 +7,7 @@ from libgemini import sol
 from libgemini import adc_errors
 
 
-def main(calibration_points, adc_range, adc_resolution, adc_channel, save):
+def main(calibration_points, adc_range, adc_resolution, invert, adc_channel, save):
     voltages = [
         n / calibration_points * adc_range
         for n in range(calibration_points + 1)
@@ -18,23 +18,27 @@ def main(calibration_points, adc_range, adc_resolution, adc_channel, save):
         for n in range(calibration_points + 1)
     ]
 
+    if invert:
+        expected_codes = [adc_resolution - 1 - code for code in expected_codes]
+
     measured_codes = []
 
-
     gem = gemini.Gemini()
-    sol = sol.Sol()
+    sol_ = sol.Sol()
 
     gem.enter_calibration_mode()
 
     # TODO: Change to "send_voltage"
-    sol.send_note(0)
-    sol.send_note(0)
-    sol.send_note(0)
+    sol_.send_voltage(0)
 
     for n in range(calibration_points + 1):
-        sol.send_note(n)
+        voltage = n / calibration_points * adc_range
+        print(f"Measuring {voltage}, expecting {expected_codes[n]}.")
+        sol_.send_voltage(voltage)
         time.sleep(0.2)
-        measured_codes.append(gem.read_adc(adc_channel))
+        result = gem.read_adc(adc_channel)
+        print(f"Got {result}, diff {result - expected_codes[n]}")
+        measured_codes.append(result)
 
     gain_error = adc_errors.calculate_avg_gain_error(expected_codes, measured_codes)
     offset_error = adc_errors.calculate_avg_offset_error(expected_codes, measured_codes, gain_error)
@@ -46,7 +50,7 @@ def main(calibration_points, adc_range, adc_resolution, adc_channel, save):
     print(f"Corrected: Gain: {corrected_gain_error}, Offset: {corrected_offset_error}")
 
     if save:
-        gem.set_adc_gain_error(int(gain_error))
+        gem.set_adc_gain_error(gain_error)
         gem.set_adc_offset_error(int(offset_error))
         print("Saved to NVM.")
 
@@ -57,8 +61,9 @@ if __name__ == "__main__":
     parser.add_argument("--adc_range", type=float, default=3.3, help="ADC range (in volts).")
     parser.add_argument("--adc_resolution", type=int, default=2**12, help="ADC resolution.")
     parser.add_argument("--adc_channel", type=int, default=0, help="Which of Gemini's ADC channels to measure.")
-    parser.add_argument("--dry_run", type=bool, action="store_true", default=False, help="Don't save the calibration values.")
+    parser.add_argument("--invert", action="store_true", default=False, help="Indicates that the bottom of the voltage range represents the top of the ADC code range")
+    parser.add_argument("--dry_run", action="store_true", default=False, help="Don't save the calibration values.")
 
     args = parser.parse_args()
 
-    main(args.calibration_points, args.adc_range, args.adc_resolution, args.adc_channel, not args.dry_run)
+    main(args.calibration_points, args.adc_range, args.adc_resolution, args.invert, args.adc_channel, not args.dry_run)
--- factory/libgemini/sol.py
+++ factory/libgemini/sol.py
@@ -1,5 +1,13 @@
 # Interface to Sol over MIDI
 
+import struct
+
+import rtmidi.midiutil
+
+SYSEX_START = 0xF0
+SYSEX_END = 0xF7
+SYSEX_MARKER = 0x78
+
 
 class Sol:
     MIDI_PORT_NAME = "Sol usb_midi.ports[0]"
@@ -9,3 +17,30 @@ class Sol:
 
     def send_note(self, number):
         self.port_out.send_message([0x90, number, 0x7F])
+
+    def send_voltage(self, voltage):
+        packed = struct.pack("f", voltage)
+        encoded = bytearray([
+            (packed[0] >> 4) & 0xF,
+            packed[0] & 0xF,
+            (packed[1] >> 4) & 0xF,
+            packed[1] & 0xF,
+            (packed[2] >> 4) & 0xF,
+            packed[2] & 0xF,
+            (packed[3] >> 4) & 0xF,
+            packed[3] & 0xF,
+        ])
+        
+        self.port_out.send_message([
+            SYSEX_START,
+            SYSEX_MARKER,
+            0x01,
+            encoded[0],
+            encoded[1],
+            encoded[2],
+            encoded[3],
+            encoded[4], 
+            encoded[5],
+            encoded[6],
+            encoded[7],
+            SYSEX_END])
