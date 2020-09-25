914a89da03b790c13db8764d762b3bdfc437583a
Thea Flowers <me@thea.codes>
2020-09-25 12:35:46 -0400

    Add calibration mode LED

--- firmware/src/gem_led_animation.c
+++ firmware/src/gem_led_animation.c
@@ -7,9 +7,11 @@
 #include "gem_waveforms.h"
 #include <stdint.h>
 
+enum gem_led_animation_mode _mode = GEM_LED_MODE_NORMAL;
+
 void gem_led_animation_init() {}
 
-void gem_led_animation_step() {
+void _gem_led_animation_step_normal() {
     uint32_t ticks = gem_get_ticks();
     for (uint8_t i = 0; i < GEM_DOTSTAR_COUNT; i++) {
         // sinadj = (sine(bright_time) + 1.0) / 2.0
@@ -22,5 +24,32 @@ void gem_led_animation_step() {
             gem_colorspace_hsv_to_rgb((ticks * 5) + (65535 / GEM_DOTSTAR_COUNT * i), 255 - value / 2, value);
         gem_dotstar_set32(i, color);
     }
+}
+
+void _gem_led_animation_step_calibration() {
+    uint32_t ticks = gem_get_ticks();
+    for (uint8_t i = 0; i < GEM_DOTSTAR_COUNT; i++) {
+        fix16_t bright_time = fix16_div(fix16_from_int(ticks / 2), F16(5000.0f));
+        fix16_t sinv = gem_sine(bright_time);
+        fix16_t sinadj = fix16_div(fix16_add(sinv, F16(1.0f)), F16(2.0f));
+        uint8_t value = fix16_to_int(fix16_mul(F16(255.0f), sinadj));
+        uint32_t color = gem_colorspace_hsv_to_rgb(0, 255 - value / 2, value);
+        gem_dotstar_set32(i, color);
+    }
+}
+
+void gem_led_animation_step() {
+    switch (_mode) {
+        case GEM_LED_MODE_NORMAL:
+            _gem_led_animation_step_normal();
+            break;
+        case GEM_LED_MODE_CALIBRATION:
+            _gem_led_animation_step_calibration();
+            break;
+        default:
+            break;
+    }
     gem_dotstar_update();
 }
+
+void gem_led_animation_set_mode(enum gem_led_animation_mode mode) { _mode = mode; }
--- firmware/src/gem_led_animation.h
+++ firmware/src/gem_led_animation.h
@@ -1,5 +1,9 @@
 #pragma once
 
+enum gem_led_animation_mode { GEM_LED_MODE_NORMAL, GEM_LED_MODE_CALIBRATION };
+
 void gem_led_animation_init();
 
 void gem_led_animation_step();
+
+void gem_led_animation_set_mode(enum gem_led_animation_mode mode);
