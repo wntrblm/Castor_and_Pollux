77c16cf8afb58d912e7d4fe0114e2e28c43de8cc
Thea Flowers <me@thea.codes>
2020-10-01 01:13:44 -0400

    Make new LED animation

--- firmware/src/gem_led_animation.c
+++ firmware/src/gem_led_animation.c
@@ -6,22 +6,52 @@
 #include "gem_systick.h"
 #include "gem_waveforms.h"
 #include <stdint.h>
+#include <stdlib.h>
 
 enum gem_led_animation_mode _mode = GEM_LED_MODE_NORMAL;
 uint32_t _last_update;
+fix16_t _phase_a = F16(0);
+uint32_t _hue_accum;
+uint32_t _hue_offsets[GEM_DOTSTAR_COUNT] = {
+    65355 / GEM_DOTSTAR_COUNT * 2,
+    65355 / GEM_DOTSTAR_COUNT * 2,
+    65355 / GEM_DOTSTAR_COUNT * 6,
+    0,
+    65355 / GEM_DOTSTAR_COUNT,
+    65355 / GEM_DOTSTAR_COUNT * 4,
+    65355 / GEM_DOTSTAR_COUNT * 3,
+};
+uint8_t _sparkles[GEM_DOTSTAR_COUNT];
 
 void gem_led_animation_init() { _last_update = gem_get_ticks(); }
 
-void _gem_led_animation_step_normal(uint32_t ticks) {
-    for (uint8_t i = 0; i < GEM_DOTSTAR_COUNT; i++) {
-        // sinadj = (sine(bright_time) + 1.0) / 2.0
-        fix16_t bright_time = fix16_div(fix16_from_int(ticks * i / 2), F16(5000.0f));
-        fix16_t sinv = gem_sine(bright_time);
-        fix16_t sinadj = fix16_div(fix16_add(sinv, F16(1.0f)), F16(2.0f));
-        // value = 255 * sinadj
-        uint8_t value = fix16_to_int(fix16_mul(F16(255.0f), sinadj));
-        uint32_t color =
-            gem_colorspace_hsv_to_rgb((ticks * 5) + (65535 / GEM_DOTSTAR_COUNT * i), 255 - value / 2, value);
+void _gem_led_animation_step_normal(uint32_t delta) {
+    _phase_a += fix16_div(fix16_from_int(delta), F16(2200.0));
+    if (_phase_a > F16(1.0))
+        _phase_a = fix16_sub(_phase_a, F16(1.0));
+
+    _hue_accum += delta * 5;
+
+    for (size_t i = 0; i < GEM_DOTSTAR_COUNT; i++) {
+        fix16_t phase_offset = fix16_div(fix16_from_int(i), F16(GEM_DOTSTAR_COUNT));
+        fix16_t sin_a = gem_sine_norm(_phase_a + phase_offset);
+        uint8_t value = 20 + fix16_to_int(fix16_mul(sin_a, F16(235)));
+        uint16_t hue = (_hue_accum + _hue_offsets[i]) % UINT16_MAX;
+        uint32_t color;
+
+        if (rand() % 500 == 0)
+            _sparkles[i] = 255;
+
+        if (_sparkles[i] == 0) {
+            color = gem_colorspace_hsv_to_rgb(hue, 255, value);
+        } else {
+            color = gem_colorspace_hsv_to_rgb(hue, 255 - _sparkles[i], value);
+            if (_sparkles[i] <= delta / 4)
+                _sparkles[i] = 0;
+            else
+                _sparkles[i] -= delta / 4;
+        }
+
         gem_dotstar_set32(i, color);
     }
 }
@@ -39,7 +69,8 @@ void _gem_led_animation_step_calibration(uint32_t ticks) {
 
 void gem_led_animation_step() {
     uint32_t ticks = gem_get_ticks();
-    if (ticks - _last_update < GEM_ANIMATION_INTERVAL) {
+    uint32_t delta = ticks - _last_update;
+    if (delta < GEM_ANIMATION_INTERVAL) {
         return;
     }
 
@@ -47,7 +78,7 @@ void gem_led_animation_step() {
 
     switch (_mode) {
         case GEM_LED_MODE_NORMAL:
-            _gem_led_animation_step_normal(ticks);
+            _gem_led_animation_step_normal(delta);
             break;
         case GEM_LED_MODE_CALIBRATION:
             _gem_led_animation_step_calibration(ticks);
--- firmware/src/gem_waveforms.h
+++ firmware/src/gem_waveforms.h
@@ -3,7 +3,9 @@
 #include "fix16.h"
 #include "math.h"
 
-static inline fix16_t gem_sine(fix16_t phase) { return fix16_sin(fix16_mul(phase, F16(2.0f * 3.14159265f))); }
+static inline fix16_t gem_sine(fix16_t phase) { return fix16_sin(fix16_mul(phase, F16(M_PI * 2.0))); }
+
+static inline fix16_t gem_sine_norm(fix16_t phase) { return fix16_div(fix16_add(gem_sine(phase), F16(1.0)), F16(2.0)); }
 
 static inline fix16_t gem_triangle(fix16_t phase) {
     /* -1.0f + fabsf(-2.0f + fmodf(phase, 1.0f) * 4.0f); */
