cbc07638903a139977fc9b83c4428e490ca5e439
Thea Flowers <me@thea.codes>
2020-10-11 01:22:39 -0400

    Add get serial number routines

---
+++ firmware/src/hw/gem_serial_number.c
@@ -0,0 +1,15 @@
+#include "gem_serial_number.h"
+
+#define SERIAL_NO_WORD_0 (*((uint32_t*)0x0080A00C))
+#define SERIAL_NO_WORD_1 (*((uint32_t*)0x0080A040))
+#define SERIAL_NO_WORD_2 (*((uint32_t*)0x0080A044))
+#define SERIAL_NO_WORD_3 (*((uint32_t*)0x0080A048))
+
+uint32_t gem_serial_number_high() {
+    return SERIAL_NO_WORD_0 << 16 | SERIAL_NO_WORD_1;
+}
+
+uint32_t gem_serial_number_low() {
+    return SERIAL_NO_WORD_2 << 16 | SERIAL_NO_WORD_3;
+}
+
---
+++ firmware/src/hw/gem_serial_number.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <stdint.h>
+
+uint32_t gem_serial_number_high();
+uint32_t gem_serial_number_low();
