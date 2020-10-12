2d109b267590391ef7b113ec472516229d94d7c0
Thea Flowers <me@thea.codes>
2020-10-11 20:23:41 -0400

    Remove unused files

--- firmware/src/gem_quant.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "gem_config.h"
-#include "math.h"
-#include "stdint.h"
-
-inline static float _gem_quant(float delta, float value) { return delta * roundf(value / delta); }
-
-inline static float gem_quant_pitch_cv(float value) { return _gem_quant(GEM_QUANT_CV_IN_STEP, value); }
-inline static float gem_quant_pitch_knob(float value) { return _gem_quant(GEM_QUANT_PITCH_KNOB_STEP, value); }
