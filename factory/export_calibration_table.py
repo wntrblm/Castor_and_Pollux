86c6a33796039eadcd63001f42f1c9b68dad2793
Thea Flowers <me@thea.codes>
2020-10-11 21:40:49 -0400

    Split param tables into two seperate tables to save ram

--- factory/export_calibration_table.py
+++ factory/export_calibration_table.py
@@ -7,7 +7,29 @@ with open("calibration-table.csv", "r") as fh:
 
     for row in reader:
         entries.append(row)
+ 
+print("""\
+#include "gem_voice_param_table.h"
+#include "fix16.h"
 
+const struct gem_voice_voltage_and_period gem_voice_voltage_and_period_table[] = {\
+""")
 
 for row in entries:
-    print(f"{{.voltage = F16({row['Input CV']}), .period_reg = {row['period reg']}, .castor_dac_code = {row['castor calibrated dac code']}, .pollux_dac_code = {row['pollux calibrated dac code']} }},")
+    print(f"  {{.voltage = F16({row['Input CV']}), .period = {row['period reg']}}},")
+
+print("""\
+};
+
+struct gem_voice_dac_codes gem_voice_dac_codes_table[] = {\
+""")
+
+for row in entries:
+    print(f"  {{.castor = {row['castor calibrated dac code']}, .pollux = {row['pollux calibrated dac code']} }},")
+
+
+print("""\
+};
+
+size_t gem_voice_param_table_len = sizeof(gem_voice_voltage_and_period_table) / sizeof(struct gem_voice_voltage_and_period);
+""")
--- firmware/src/gem_voice_params.h
+++ firmware/src/gem_voice_params.h
@@ -10,12 +10,28 @@
    voltage is configured by the DAC code.
 */
 
-struct gem_voice_params {
+/* The configuration values for this struct are generated ahead of time and are static,
+   so they're a separate struct. */
+struct gem_voice_voltage_and_period {
     fix16_t voltage;
-    uint32_t period_reg;
-    uint16_t castor_dac_code;
-    uint16_t pollux_dac_code;
+    uint32_t period;
+};
+
+/* The configuration values for this struct are calibrated after assembly, therefore
+   they are stored in NVM so they're a separate struct. */
+struct gem_voice_dac_codes {
+    uint16_t castor;
+    uint16_t pollux;
+};
+
+struct gem_voice_params {
+    struct gem_voice_voltage_and_period voltage_and_period;
+    struct gem_voice_dac_codes dac_codes;
 };
 
 void gem_voice_params_from_cv(
-    struct gem_voice_params* table, size_t table_len, fix16_t voltage, struct gem_voice_params* out);
+    const struct gem_voice_voltage_and_period* volt_table,
+    struct gem_voice_dac_codes* dac_table,
+    size_t table_len,
+    fix16_t voltage,
+    struct gem_voice_params* out);
