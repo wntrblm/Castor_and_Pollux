b984a34c16081081bfdf495fd9056b8d978d83bb
Thea Flowers <me@thea.codes>
2020-10-19 02:55:31 -0400

    Add untested save/load dac code lut

--- firmware/src/gem_voice_param_table.h
+++ firmware/src/gem_voice_param_table.h
@@ -5,3 +5,6 @@
 extern const struct gem_voice_voltage_and_period gem_voice_voltage_and_period_table[];
 extern struct gem_voice_dac_codes gem_voice_dac_codes_table[];
 extern size_t gem_voice_param_table_len;
+
+void gem_load_dac_codes_table();
+void gem_save_dac_codes_table();
