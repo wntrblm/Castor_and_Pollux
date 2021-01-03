27e02a285aa47ff5be584ca4a0b6fcd731fbf435
Thea Flowers <me@thea.codes>
2021-01-02 22:55:41 -0500

    Add teeth encoding libraries

---
+++ firmware/src/lib/teeth.c
@@ -0,0 +1,120 @@
+#include <assert.h>
+#include "teeth.h"
+
+
+void teeth_encode(uint8_t* src, size_t src_len, uint8_t* dst) {
+    size_t src_idx = 0;
+    size_t dst_idx = 0;
+
+    while(src_idx < src_len) {
+        // There's 4 or more bytes left
+        if (src_idx + 4 <= src_len) {
+            // First byte carries the leftover bits.
+            dst[dst_idx] = 0x40 | ((src[src_idx] & 0x80) >> 4) | ((src[src_idx + 1] & 0x80) >> 5) | ((src[src_idx + 2] & 0x80) >> 6) | ((src[src_idx + 3] & 0x80) >> 7);
+            // Subsequent bytes carry their lower 7 bits.
+            dst[dst_idx+1] = src[src_idx] & 0x7F;
+            dst[dst_idx+2] = src[src_idx+1] & 0x7F;
+            dst[dst_idx+3] = src[src_idx+2] & 0x7F;
+            dst[dst_idx+4] = src[src_idx+3] & 0x7F;
+            dst_idx += 5;
+            src_idx += 4;
+        }
+        // There's only 3 bytes left
+        else if (src_idx + 3 == src_len) {
+            dst[dst_idx] = 0x30 | ((src[src_idx] & 0x80) >> 4) | ((src[src_idx + 1] & 0x80) >> 5) | ((src[src_idx + 2] & 0x80) >> 6);
+            dst[dst_idx+1] = src[src_idx] & 0x7F;
+            dst[dst_idx+2] = src[src_idx+1] & 0x7F;
+            dst[dst_idx+3] = src[src_idx+2] & 0x7F;
+            dst_idx += 4;
+            src_idx += 3;
+        }
+        // There's only 2 bytes left
+        else if (src_idx + 2 == src_len) {
+            dst[dst_idx] = 0x20 | ((src[src_idx] & 0x80) >> 4) | ((src[src_idx + 1] & 0x80) >> 5);
+            dst[dst_idx+1] = src[src_idx] & 0x7F;
+            dst[dst_idx+2] = src[src_idx+1] & 0x7F;
+            dst_idx += 3;
+            src_idx += 2;
+        }
+        // There's only 1 byte left
+        else if (src_idx + 1 == src_len) {
+            dst[dst_idx] = 0x10 | ((src[src_idx] & 0x80) >> 4);
+            dst[dst_idx+1] = src[src_idx] & 0x7F;
+            dst_idx += 2;
+            src_idx += 1;
+        }
+        else {
+            break;
+        }
+    }
+}
+
+
+size_t teeth_decode(uint8_t* src, size_t src_len, uint8_t* dst) {
+    assert(src_len % 5 == 0);
+    size_t src_idx = 0;
+    size_t dst_idx = 0;
+
+    while (src_idx < src_len) {
+        dst[dst_idx] = (src[src_idx] & 0x8) << 4 | src[src_idx+1];
+        dst[dst_idx+1] = (src[src_idx] & 0x4) << 5 | src[src_idx+2];
+        dst[dst_idx+2] = (src[src_idx] & 0x2) << 6 | src[src_idx+3];
+        dst[dst_idx+3] = (src[src_idx] & 0x1) << 7 | src[src_idx+4];
+        uint8_t len_marker = src[src_idx] >> 4;
+        dst_idx += len_marker;
+        src_idx += 5;
+    }
+
+    return dst_idx;
+}
+
+
+#ifdef TEETH_TEST
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    printf("Teeth encode/decode tests\n");
+
+    srand(time(NULL));
+
+    uint8_t source_buf[128];
+    uint8_t dest_buf[TEETH_ENCODED_LENGTH(128)];
+    uint8_t result_buf[128];
+
+    printf("Random data, fixed length buffers...");
+    for(size_t n = 0; n < 100; n++) {
+        for(size_t i = 0; i < 128; i++) {
+            source_buf[i] = (uint8_t)rand();
+        }
+
+        teeth_encode(source_buf, 128, dest_buf);
+        teeth_decode(dest_buf, TEETH_ENCODED_LENGTH(128), result_buf);
+
+        assert(memcmp(source_buf, result_buf, 128) == 0);
+    }
+    printf("ok!\n");
+
+    printf("Random data, random length buffers...");
+    for(size_t n = 0; n < 100; n++) {
+        size_t len = rand() % 128;
+        for(size_t i = 0; i < len; i++) {
+            source_buf[i] = (uint8_t)rand();
+        }
+
+        teeth_encode(source_buf, len, dest_buf);
+        teeth_decode(dest_buf, TEETH_ENCODED_LENGTH(len), result_buf);
+
+        assert(memcmp(source_buf, result_buf, len) == 0);
+    }
+
+    printf("ok!\n");
+
+    printf("All done :)\n");
+
+}
+
+#endif
---
+++ firmware/src/lib/teeth.h
@@ -0,0 +1,49 @@
+#pragma once
+
+/* Teeth encoding/decoding routines
+
+> "What the heck is Teeth encoding?"
+
+It's a really dumb encoding scheme designed to deal with sending data over
+MIDI SysEx which is limited to 7-bit values. It encodes each 32-bit (4 byte)
+value using 5 bytes, therefore, it has a 25% overhead. It's designed to
+be deterministic so that C implementations can avoid dynamic allocation -
+regardless of the number of items in the encoder input, the encoded output
+is *always* a deterministic multiple of 5.
+
+The basic scheme is that the following 32-bit value:
+
+0x1AAA AAAA
+0x2BBB BBBB
+0x3CCC CCCC
+0x4DDD DDDD
+
+Gets encoded as:
+
+0x4A <--- header byte
+0x0AAA AAAA
+0x0BBB BBBB
+0x0CCC CCCC
+0x0DDD DDDD
+
+The header byte's upper nibble is number of following bytes, its max value is
+4. This allows encoding and decoding arrays that aren't a multiple of 4 bytes
+in length and preserving the length during decoding.
+
+The second nibble contains the most significant bit for each of the
+directly following bytes as 0b1234.
+
+> "Why the HECK did you name it Teeth?"
+
+It works on 32-bit values and humans happen to have 32 teeth, also, it takes
+one more byte than usual to encode so it's an *overbyte*...
+
+*/
+
+#include <stdint.h>
+#include <stddef.h>
+
+#define TEETH_ENCODED_LENGTH(src_len) (((src_len + 4 - 1) / 4) * 5)
+
+void teeth_encode(uint8_t* src, size_t src_len, uint8_t* dst);
+size_t teeth_decode(uint8_t* src, size_t src_len, uint8_t* dst);
