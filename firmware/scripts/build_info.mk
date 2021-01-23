730ee2920465e4345a3ff83a583d61ab159b96f0
Thea Flowers <me@thea.codes>
2021-01-23 13:30:06 -0500

    Print out build ID during build

--- firmware/scripts/build_info.mk
+++ firmware/scripts/build_info.mk
@@ -1,6 +1,6 @@
 # Build information - used by gem_build_info.{h,c}
 
-BUILD_INFO_COMPILER=$(CC)
+BUILD_INFO_COMPILER = $(CC) $(shell $(CC) -dumpversion)
 # Looks like: 12.24.2020
 BUILD_INFO_RELEASE = $(shell git describe --tags --abbrev=0)
 # Looks like: 12 24 2020
@@ -11,9 +11,13 @@ BUILD_INFO_RELEASE_DAY = $(word 1,$(BUILD_INFO_RELEASE_PARTS))
 # Looks like: 12.24.2020-46-gb77c425-dirty
 BUILD_INFO_REVISION = $(shell git describe --always --tags --dirty)
 # Looks like: 20/01/2021 22:34 UTC
-BUILD_INFO_DATE = $(shell date -u +"%d/%m/%Y %H:%M UTC")
+BUILD_INFO_DATE = $(shell date -u +"%m/%d/%Y %H:%M UTC")
 # Looks like: stargirl@stargirls-mbp.lan
 BUILD_INFO_MACHINE = $(shell echo `whoami`@`hostname`)
+
+# The whole enchilada. Looks like:
+# 12.24.2020-83-g786ee18-dirty (Debug) on 01/23/2021 18:28 UTC with arm-none-eabi-gcc 10.2.1 stargirl@stargirls-mbp.lan
+BUILD_INFO_STRING = $(BUILD_INFO_REVISION) ($(BUILD_TYPE)) on $(BUILD_INFO_DATE) with $(BUILD_INFO_COMPILER) $(BUILD_INFO_MACHINE)
 
 DEFINES +=\
 	-DBUILD_INFO_COMPILER="\"$(BUILD_INFO_COMPILER)\"" \
@@ -23,4 +27,5 @@ DEFINES +=\
 	-DBUILD_INFO_RELEASE_MONTH="$(BUILD_INFO_RELEASE_MONTH)" \
 	-DBUILD_INFO_RELEASE_DAY="$(BUILD_INFO_RELEASE_DAY)" \
 	-DBUILD_INFO_DATE="\"$(BUILD_INFO_DATE)\"" \
-	-DBUILD_INFO_MACHINE="\"$(BUILD_INFO_MACHINE)\""
+	-DBUILD_INFO_MACHINE="\"$(BUILD_INFO_MACHINE)\"" \
+	-DBUILD_INFO_STRING="\"$(BUILD_INFO_STRING)\""
