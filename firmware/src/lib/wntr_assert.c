d3676a82cd36efbe94152c4bff408663b40d2fab
Thea Flowers <me@thea.codes>
2021-01-26 18:14:51 -0500

    meta: add missing license header

--- firmware/src/lib/wntr_assert.c
+++ firmware/src/lib/wntr_assert.c
@@ -1,3 +1,9 @@
+/*
+    Copyright (c) 2021 Alethea Katherine Flowers.
+    Published under the standard MIT License.
+    Full text available at: https://opensource.org/licenses/MIT
+*/
+
 #include "wntr_assert.h"
 #include "printf.h"
 #include "sam.h"
