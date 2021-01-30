8b49bbb43ba58d92d77957650179a5c2fd3dc23b
Thea Flowers <me@thea.codes>
2021-01-29 22:09:10 -0500

    Add note/next command to shell for quickly testing ramp

--- factory/shell.py
+++ factory/shell.py
@@ -1,5 +1,6 @@
 import readline
-from libgemini import gemini
+
+from libgemini import gemini, reference_calibration
 
 readline.parse_and_bind("tab: complete")
 
@@ -8,12 +9,47 @@ gem.enter_calibration_mode()
 
 settings = None
 
+
+def _frequency_to_timer_period(frequency):
+    return round(((8_000_000 / 1) / frequency) - 1)
+
+
+def _midi_note_to_frequency(note):
+    return pow(2, (note - 69) / 12) * 440
+
+
+def _estimate_charge_code(frequency):
+    return min(round(10 * frequency / 5_000 / 3.3 * 4095), 4095)
+
+
+midi_note = 11
+
+
 while True:
     cmd, *vals = input("> ").strip().split()
     vals = [int(val) for val in vals]
     val = vals[0] if vals else None
 
-    if cmd == "read_adc":
+    if cmd == "note":
+        freq = vals[0]
+        charge_code = vals[1]
+        period = _frequency_to_timer_period(freq)
+        print(f"Period: {period}")
+        gem.set_period(0, period)
+        gem.set_dac(0, charge_code, 0)
+
+    if cmd == "next":
+        midi_note += 2
+        freq = _midi_note_to_frequency(midi_note)
+        period = _frequency_to_timer_period(freq)
+        charge_code = _estimate_charge_code(freq)
+        print(f"Note: {midi_note}, Freq: {freq}, Charge code: {charge_code}")
+        gem.set_period(0, period)
+        gem.set_dac(0, charge_code, 0)
+        gem.set_period(1, period)
+        gem.set_dac(2, reference_calibration.pollux[period], 1)
+
+    elif cmd == "read_adc":
         print(gem.read_adc(val))
 
     elif cmd == "set_dac":
