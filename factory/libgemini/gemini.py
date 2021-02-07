fc99707d6bc5e830d2b60f8738b87fb632fd7da4
Thea Flowers <me@thea.codes>
2021-02-07 12:10:37 -0500

    factory: use wintertools.midi for Gemini

--- factory/libgemini/gemini.py
+++ factory/libgemini/gemini.py
@@ -7,22 +7,9 @@
 import enum
 import struct
 
-import rtmidi.midiutil
-from wintertools import log, teeth
+from wintertools import log, midi, teeth
 
 from libgemini import gem_monitor_update, gem_settings
-
-SYSEX_START = 0xF0
-SYSEX_END = 0xF7
-SYSEX_MARKER = 0x77
-
-
-def _wait_for_message(port_in):
-    while True:
-        msg = port_in.get_message()
-        if msg:
-            msg, _ = msg
-            return msg
 
 
 def _fix16(val):
@@ -51,70 +38,40 @@ class SysExCommands(enum.IntEnum):
     MONITOR = 0x10
 
 
-class Gemini:
+class Gemini(midi.MIDIDevice):
     MIDI_PORT_NAME = "Gemini"
+    SYSEX_MARKER = 0x77
 
     def __init__(self):
-        self.port_in, _ = rtmidi.midiutil.open_midiport(
-            self.MIDI_PORT_NAME, type_="input"
-        )
-        self.port_in.ignore_types(sysex=False)
-        self.port_out, _ = rtmidi.midiutil.open_midiport(
-            self.MIDI_PORT_NAME, type_="output"
-        )
+        super().__init__()
         self.version = None
         self.serial_number = None
 
-    def close(self):
-        self.port_in.close_port()
-        self.port_out.close_port()
-
-    def _sysex(self, command, data=None, response=False, encode=False, decode=False):
-        if data is None:
-            data = []
-
-        if encode:
-            data = teeth.teeth_encode(data)
-
-        self.port_out.send_message(
-            [SYSEX_START, SYSEX_MARKER, command] + list(data) + [SYSEX_END]
-        )
-
-        if response:
-            result = _wait_for_message(self.port_in)
-
-            if decode:
-                return teeth.teeth_decode(result[3:-1])
-
-            return result
-
     def enter_calibration_mode(self):
-        resp = self._sysex(SysExCommands.HELLO, response=True)
+        resp = self.sysex(SysExCommands.HELLO, response=True)
         self.version = bytearray(resp[3:-1]).decode("ascii")
         log.info(f"Gemini version: {self.version}")
 
-        resp = self._sysex(SysExCommands.GET_SERIAL_NUMBER, response=True)
+        resp = self.sysex(SysExCommands.GET_SERIAL_NUMBER, response=True)
         self.serial_number = teeth.teeth_decode(resp[3:-1]).hex()
         log.info(f"Serial number: {self.serial_number}")
 
     def read_adc(self, ch):
-        resp = self._sysex(
-            SysExCommands.READ_ADC, data=[ch], response=True, decode=True
-        )
+        resp = self.sysex(SysExCommands.READ_ADC, data=[ch], response=True, decode=True)
         (val,) = struct.unpack(">H", resp)
         return val
 
     def set_dac(self, ch, val, vref):
         data = struct.pack(">BHB", ch, val, vref)
-        self._sysex(SysExCommands.SET_DAC, data=data, encode=True)
+        self.sysex(SysExCommands.SET_DAC, data=data, encode=True)
 
     def set_period(self, ch, val):
         data = struct.pack(">BI", ch, val)
-        self._sysex(SysExCommands.SET_FREQ, data=data, encode=True)
+        self.sysex(SysExCommands.SET_FREQ, data=data, encode=True)
 
     def set_adc_gain_error_int(self, val):
         data = struct.pack(">H", val)
-        self._sysex(SysExCommands.WRITE_ADC_GAIN, data=data, encode=True)
+        self.sysex(SysExCommands.WRITE_ADC_GAIN, data=data, encode=True)
 
     def set_adc_gain_error(self, val):
         val = int(val * 2048)
@@ -122,16 +79,16 @@ class Gemini:
 
     def set_adc_offset_error(self, val):
         data = struct.pack(">h", val)
-        self._sysex(SysExCommands.WRITE_ADC_OFFSET, data=data, encode=True)
+        self.sysex(SysExCommands.WRITE_ADC_OFFSET, data=data, encode=True)
 
     def disable_adc_error_correction(self):
-        self._sysex(SysExCommands.DISABLE_ADC_CORR)
+        self.sysex(SysExCommands.DISABLE_ADC_CORR)
 
     def enable_adc_error_correction(self):
-        self._sysex(SysExCommands.ENABLE_ADC_CORR)
+        self.sysex(SysExCommands.ENABLE_ADC_CORR)
 
     def reset_settings(self):
-        self._sysex(SysExCommands.RESET_SETTINGS)
+        self.sysex(SysExCommands.RESET_SETTINGS)
 
     CHUNK_SIZE = 10
     SETTINGS_ENCODED_LEN = teeth.teeth_encoded_length(
@@ -143,7 +100,7 @@ class Gemini:
         settings_encoded = bytearray(self.SETTINGS_ENCODED_LEN)
 
         for n in range(self.SETTINGS_CHUNKS):
-            data = self._sysex(SysExCommands.READ_SETTINGS, data=[n], response=True)
+            data = self.sysex(SysExCommands.READ_SETTINGS, data=[n], response=True)
             assert len(data) == self.CHUNK_SIZE + 4
             settings_encoded[
                 self.CHUNK_SIZE * n : self.CHUNK_SIZE * n + self.CHUNK_SIZE
@@ -164,29 +121,27 @@ class Gemini:
                     self.CHUNK_SIZE * n : self.CHUNK_SIZE * n + self.CHUNK_SIZE
                 ]
             )
-            self._sysex(SysExCommands.WRITE_SETTINGS, data=[n] + chunk, response=True)
+            self.sysex(SysExCommands.WRITE_SETTINGS, data=[n] + chunk, response=True)
 
     def write_lut_entry(self, entry, period, castor, pollux):
         data = struct.pack(">BIHH", entry, period, castor, pollux)
-        self._sysex(
-            SysExCommands.WRITE_LUT_ENTRY, data=data, encode=True, response=True
-        )
+        self.sysex(SysExCommands.WRITE_LUT_ENTRY, data=data, encode=True, response=True)
 
     def write_lut(self):
-        self._sysex(SysExCommands.WRITE_LUT)
+        self.sysex(SysExCommands.WRITE_LUT)
 
     def erase_lut(self):
-        self._sysex(SysExCommands.ERASE_LUT)
+        self.sysex(SysExCommands.ERASE_LUT)
 
     def enable_monitor(self):
-        self._sysex(SysExCommands.MONITOR, data=[1])
+        self.sysex(SysExCommands.MONITOR, data=[1])
 
     def disable_monitor(self):
-        self._sysex(SysExCommands.MONITOR, data=[0])
+        self.sysex(SysExCommands.MONITOR, data=[0])
 
     def monitor(self):
         try:
-            result = _wait_for_message(self.port_in)
+            result = self.wait_for_message()
             decoded = teeth.teeth_decode(result[3:-1])
             unpacked = gem_monitor_update.GemMonitorUpdate.unpack(decoded)
             return unpacked
