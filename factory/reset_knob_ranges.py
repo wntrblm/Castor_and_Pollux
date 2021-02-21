81738122da7af4ff8c20fbcc8b13dcc3d0335528
Thea Flowers <me@thea.codes>
2021-02-21 01:04:22 -0500

    factory: Add script to reset knob range settings

---
+++ factory/reset_knob_ranges.py
@@ -0,0 +1,16 @@
+from libgemini import gem_settings, gemini
+
+gem = gemini.Gemini()
+
+default_settings = gem_settings.GemSettings()
+
+settings = gem.read_settings()
+
+settings.castor_knob_min = default_settings.castor_knob_min
+settings.castor_knob_max = default_settings.castor_knob_max
+settings.pollux_knob_min = default_settings.pollux_knob_min
+settings.pollux_knob_max = default_settings.pollux_knob_max
+
+gem.save_settings(settings)
+
+print(gem.read_settings())
