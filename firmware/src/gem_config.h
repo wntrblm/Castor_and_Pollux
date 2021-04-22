wntrblm/Castor_and_Pollux

commit c8cc7b213a90665e6298770d26a5dc0bb2473756

firmware: Use some libwinter macros instead of bare numbers

firmware/src/gem_config.h
@@ -46,15 +46,15 @@
     scan.
 */
 static const struct GemADCInput gem_adc_inputs[] = {
-    {0, 6, ADC_INPUTCTRL_MUXPOS_PIN6},   // CV A
-    {1, 3, ADC_INPUTCTRL_MUXPOS_PIN11},  // CV A Pot
-    {0, 7, ADC_INPUTCTRL_MUXPOS_PIN7},   // CV B
-    {1, 2, ADC_INPUTCTRL_MUXPOS_PIN10},  // CV B Pot
-    {0, 5, ADC_INPUTCTRL_MUXPOS_PIN5},   // Duty A
-    {0, 8, ADC_INPUTCTRL_MUXPOS_PIN16},  // Duty A Pot
-    {1, 9, ADC_INPUTCTRL_MUXPOS_PIN3},   // Duty B
-    {0, 9, ADC_INPUTCTRL_MUXPOS_PIN17},  // Duty B Pot
-    {0, 2, ADC_INPUTCTRL_MUXPOS_PIN0},   // Chorus pot
+    {WNTR_PORT_A, 6, ADC_INPUTCTRL_MUXPOS_PIN6},   // CV A
+    {WNTR_PORT_B, 3, ADC_INPUTCTRL_MUXPOS_PIN11},  // CV A Pot
+    {WNTR_PORT_A, 7, ADC_INPUTCTRL_MUXPOS_PIN7},   // CV B
+    {WNTR_PORT_B, 2, ADC_INPUTCTRL_MUXPOS_PIN10},  // CV B Pot
+    {WNTR_PORT_A, 5, ADC_INPUTCTRL_MUXPOS_PIN5},   // Duty A
+    {WNTR_PORT_A, 8, ADC_INPUTCTRL_MUXPOS_PIN16},  // Duty A Pot
+    {WNTR_PORT_B, 9, ADC_INPUTCTRL_MUXPOS_PIN3},   // Duty B
+    {WNTR_PORT_A, 9, ADC_INPUTCTRL_MUXPOS_PIN17},  // Duty B Pot
+    {WNTR_PORT_A, 2, ADC_INPUTCTRL_MUXPOS_PIN0},   // Chorus pot
 };
 
 /* Gives useful names to the inputs above. */
@@ -82,12 +82,12 @@ enum GemADCChannel {
     TCC1 WO1 / PA11 for Pollux (rev 1)
 */
 
-#define GEM_TCC0_PIN_PORT 0
+#define GEM_TCC0_PIN_PORT WNTR_PORT_A
 #define GEM_TCC0_PIN 17
 #define GEM_TCC0_PIN_FUNC PORT_PMUX_PMUXO_F
 #define GEM_TCC0_WO 3
 
-#define GEM_TCC1_PIN_PORT 0
+#define GEM_TCC1_PIN_PORT WNTR_PORT_B
 #define GEM_TCC1_PIN 11
 #define GEM_TCC1_PIN_FUNC PORT_PMUX_PMUXO_E
 #define GEM_TCC1_WO 1
@@ -104,10 +104,10 @@ enum GemADCChannel {
 #define GEM_I2C_SERCOM SERCOM1
 #define GEM_I2C_SERCOM_APBCMASK PM_APBCMASK_SERCOM1
 #define GEM_I2C_GCLK_CLKCTRL_ID GCLK_CLKCTRL_ID_SERCOM1_CORE
-#define GEM_I2C_PAD_0_PORT 0
+#define GEM_I2C_PAD_0_PORT WNTR_PORT_A
 #define GEM_I2C_PAD_0_PIN 0
 #define GEM_I2C_PAD_0_PIN_FUNC WNTR_PMUX_D
-#define GEM_I2C_PAD_1_PORT 0
+#define GEM_I2C_PAD_1_PORT WNTR_PORT_A
 #define GEM_I2C_PAD_1_PIN 1
 #define GEM_I2C_PAD_1_PIN_FUNC WNTR_PMUX_D
 #define GEM_I2C_WAIT_TIMEOUT 100000
@@ -124,10 +124,10 @@ enum GemADCChannel {
 #define GEM_SPI_PHASE 0
 /* Pad 2 is clock, pad 3 is data */
 #define GEM_SPI_DOPO 0x1
-#define GEM_SPI_SCK_PORT 1
+#define GEM_SPI_SCK_PORT WNTR_PORT_B
 #define GEM_SPI_SCK_PIN 23
 #define GEM_SPI_SCK_PIN_FUNC WNTR_PMUX_D
-#define GEM_SPI_SDO_PORT 1
+#define GEM_SPI_SDO_PORT WNTR_PORT_B
 #define GEM_SPI_SDO_PIN 22
 #define GEM_SPI_SDO_PIN_FUNC WNTR_PMUX_D
 

commit 84eb255389d7b658e2c7bb55ca566b45e89934da

firmware: fix incorrect port for TCC1, introduced in c8cc7b213

firmware/src/gem_config.h
@@ -87,7 +87,7 @@ enum GemADCChannel {
 #define GEM_TCC0_PIN_FUNC PORT_PMUX_PMUXO_F
 #define GEM_TCC0_WO 3
 
-#define GEM_TCC1_PIN_PORT WNTR_PORT_B
+#define GEM_TCC1_PIN_PORT WNTR_PORT_A
 #define GEM_TCC1_PIN 11
 #define GEM_TCC1_PIN_FUNC PORT_PMUX_PMUXO_E
 #define GEM_TCC1_WO 1