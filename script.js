1d4e9dc7c432d795fcc45d5816e9dabd5e0929b7
Thea Flowers <me@thea.codes>
2021-05-07 21:37:13 -0500

    Deployed 56d9a8a with MkDocs version: 1.1.2

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-(function(){
+document.addEventListener("DOMContentLoaded", () => {
     "use strict";
 
-    let nav_expand_button = document.querySelector("button.navbar-toggler");
+    /* Setup hamburger menus. */
+    document.querySelectorAll(".navbar-burger").forEach((el) => {
+        el.addEventListener("click", () => {
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
 
-    if(nav_expand_button === null) {
-        return;
-    }
-
-    let nav_target = document.querySelector(nav_expand_button.dataset.target);
-    console.log(nav_expand_button);
-    console.log(nav_target);
-    console.log(nav_expand_button.dataset.target);
-
-    nav_expand_button.addEventListener("click", function() {
-        nav_target.classList.toggle("show");
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle("is-active");
+            $target.classList.toggle("is-active");
+        });
     });
-})();
+
+    /* Check for copy-to-clipboard buttons */
+    document.querySelectorAll("button[data-clipboard-copy-target]").forEach((el) => {
+        el.addEventListener("click", () => {
+            const target = document.getElementById(el.dataset.clipboardCopyTarget);
+            const originalInnerHtml = el.innerHTML;
+
+            navigator.clipboard.writeText(target.value);
+            el.innerHTML = '<span class="material-icons">done</span>';
+            setTimeout(() => {el.innerHTML = originalInnerHtml;}, 1000);
+        });
+    });
+});
